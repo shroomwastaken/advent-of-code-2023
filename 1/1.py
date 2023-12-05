2e04acb0d2d81b16a23861d876436b7b3b524334
shroomwastaken <xeoplay123456@gmail.com>
2023-12-05 16:16:41 +0300

    removed extra print

--- 1/1.py
+++ 1/1.py
@@ -4,6 +4,5 @@ with open("1\input.txt", "r") as f:
     strings = f.readlines()
 for string in strings:
     digits = ''.join(re.findall('\d+', string))
-    print(string + f": first: {digits[0]}, last: {digits[-1]}, num: {int(digits[0]) * 10 + int(digits[-1])}")
     s += int(digits[0]) * 10 + int(digits[-1])
 print(s)
