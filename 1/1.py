409a147a6779ef1e609fceeb2d26a232b47a84d0
shroomwastaken <xeoplay123456@gmail.com>
2023-12-06 10:32:07 +0300

    fixed slash

--- 1/1.py
+++ 1/1.py
@@ -1,6 +1,6 @@
 import re
 s = 0
-with open("1\input.txt", "r") as f:
+with open("1/input.txt", "r") as f:
     strings = f.readlines()
 for string in strings:
     digits = ''.join(re.findall('\d+', string))
